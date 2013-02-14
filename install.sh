472250592ab499cf5329e3d9600adad33788423e
s4mur4i <admin@szamuraj.com>
2013-02-14 23:25:19 +0100

    Nightly build. Updated packages list

--- install.sh
+++ install.sh
@@ -82,6 +82,14 @@ if [[ ! -b $TARGET ]]; then
     echo "Target is not a block device."
     exit 1
 fi
+echo "Pretest if target mounted"
+ret=$(check_mount)
+if [ $ret -eq 1 ]; then
+    echo "Not mounted continue."
+else
+    echo "Target mounted, umounting."
+    do_umount
+fi
 echo "Zeroing Disk first"
 dd if=/dev/zero of=$TARGET bs=1M >$OUT 2>&1
 echo "Done, Now creating partitions."
@@ -240,7 +248,6 @@ $INSTALL /rpm/yum-3.2.29-30.el6.centos.noarch.rpm \
 		/rpm/yum-plugin-fastestmirror-1.1.30-14.el6.noarch.rpm \
 		/rpm/yum-presto-0.6.2-1.el6.noarch.rpm \
 		/rpm/rpm-python-4.8.0-27.el6.x86_64.rpm \
-		/rpm/expat-2.0.1-11.el6_2.x86_64.rpm \
 		/rpm/libxml2-2.7.6-8.el6_3.3.x86_64.rpm \
 		/rpm/libxml2-python-2.7.6-8.el6_3.3.x86_64.rpm \
 		/rpm/python-urlgrabber-3.9.1-8.el6.noarch.rpm \
@@ -251,10 +258,92 @@ $INSTALL /rpm/yum-3.2.29-30.el6.centos.noarch.rpm \
         /rpm/pth-2.0.7-9.3.el6.x86_64.rpm \
         /rpm/deltarpm-3.5-0.5.20090913git.el6.x86_64.rpm \
         /rpm/python-pycurl-7.19.0-8.el6.x86_64.rpm \
+        /rpm/libgcrypt-1.4.5-9.el6_2.2.x86_64.rpm \
+        /rpm/pinentry-0.7.6-6.el6.x86_64.rpm \
 		/rpm/python-iniparse-0.3.1-2.1.el6.noarch.rpm
 		
 $INSTALL /rpm/rootfiles-8.1-6.1.el6.noarch.rpm \
         /rpm/file-libs-5.04-13.el6.x86_64.rpm
+echo "Done installing base rpm-s."
+chroot /target pwconv
+echo "Installing further packages."
+$INSTALL /rpm/device-mapper-event-1.02.74-10.el6.x86_64.rpm \
+		/rpm/device-mapper-event-libs-1.02.74-10.el6.x86_64.rpm \
+		/rpm/device-mapper-multipath-0.4.9-56.el6_3.1.x86_64.rpm \
+		/rpm/device-mapper-multipath-libs-0.4.9-56.el6_3.1.x86_64.rpm \
+		/rpm/dracut-kernel-004-284.el6_3.noarch.rpm \
+		/rpm/kernel-2.6.32-279.9.1.el6.x86_64.rpm \
+		/rpm/kernel-2.6.32-279.el6.x86_64.rpm \
+		/rpm/kernel-firmware-2.6.32-279.9.1.el6.noarch.rpm \
+		/rpm/kpartx-0.4.9-56.el6_3.1.x86_64.rpm \
+		/rpm/lvm2-2.02.95-10.el6.x86_64.rpm \
+		/rpm/lvm2-libs-2.02.95-10.el6.x86_64.rpm \
+		/rpm/selinux-policy-targeted-3.7.19-155.el6_3.4.noarch.rpm \
+		/rpm/startup-notification-0.10-2.1.el6.x86_64.rpm \
+        /rpm/libaio-0.3.107-10.el6.x86_64.rpm \
+        /rpm/dracut-004-284.el6_3.noarch.rpm \
+        /rpm/dracut-kernel-004-284.el6_3.noarch.rpm \
+        /rpm/dracut-network-004-284.el6_3.noarch.rpm \
+        /rpm/grubby-7.0.15-3.el6.x86_64.rpm \
+        /rpm/libselinux-python-2.0.94-5.3.el6.x86_64.rpm \
+        /rpm/libselinux-utils-2.0.94-5.3.el6.x86_64.rpm \
+        /rpm/selinux-policy-3.7.19-155.el6_3.4.noarch.rpm \
+        /rpm/selinux-policy-targeted-3.7.19-155.el6_3.4.noarch.rpm \
+        /rpm/bridge-utils-1.2-9.el6.x86_64.rpm \
+        /rpm/bzip2-1.0.5-7.el6_0.x86_64.rpm \
+        /rpm/bzip2-libs-1.0.5-7.el6_0.x86_64.rpm \
+        /rpm/ConsoleKit-x11-0.4.1-3.el6.x86_64.rpm \
+        /rpm/control-center-filesystem-2.28.1-37.el6.x86_64.rpm \
+        /rpm/dash-0.5.5.1-3.1.el6.x86_64.rpm \
+        /rpm/dbus-x11-1.2.24-7.el6_3.x86_64.rpm \
+        /rpm/desktop-file-utils-0.15-9.el6.x86_64.rpm \
+        /rpm/DeviceKit-power-014-3.el6.x86_64.rpm \
+        /rpm/device-mapper-1.02.74-10.el6.x86_64.rpm \
+        /rpm/device-mapper-event-1.02.74-10.el6.x86_64.rpm \
+        /rpm/device-mapper-event-libs-1.02.74-10.el6.x86_64.rpm \
+        /rpm/device-mapper-libs-1.02.74-10.el6.x86_64.rpm \
+        /rpm/device-mapper-multipath-0.4.9-56.el6_3.1.x86_64.rpm \
+        /rpm/device-mapper-multipath-libs-0.4.9-56.el6_3.1.x86_64.rpm \
+        /rpm/dhclient-4.1.1-31.0.1.P1.el6.centos.1.x86_64.rpm \
+        /rpm/file-5.04-13.el6.x86_64.rpm \
+        /rpm/file-libs-5.04-13.el6.x86_64.rpm \
+        /rpm/iscsi-initiator-utils-6.2.0.872-41.el6.x86_64.rpm \
+        /rpm/kbd-1.15-11.el6.x86_64.rpm \
+        /rpm/kbd-misc-1.15-11.el6.noarch.rpm \
+        /rpm/libgnomekbd-2.28.2-2.el6.x86_64.rpm \
+        /rpm/libsndfile-1.0.20-5.el6.x86_64.rpm \
+        /rpm/libxcb-1.5-1.el6.x86_64.rpm \
+        /rpm/libxkbfile-1.0.6-1.1.el6.x86_64.rpm \
+        /rpm/m4-1.4.13-5.el6.x86_64.rpm \
+        /rpm/nfs-utils-1.2.3-26.el6.x86_64.rpm \
+        /rpm/nfs-utils-lib-1.1.5-4.el6.x86_64.rpm \
+        /rpm/plymouth-0.8.3-24.el6.centos.x86_64.rpm \
+        /rpm/plymouth-core-libs-0.8.3-24.el6.centos.x86_64.rpm \
+        /rpm/plymouth-gdm-hooks-0.8.3-24.el6.centos.x86_64.rpm \
+        /rpm/plymouth-scripts-0.8.3-24.el6.centos.x86_64.rpm \
+        /rpm/plymouth-utils-0.8.3-24.el6.centos.x86_64.rpm \
+        /rpm/rootfiles-8.1-6.1.el6.noarch.rpm \
+        /rpm/rpcbind-0.2.0-9.el6.x86_64.rpm \
+        /rpm/spice-glib-0.11-11.el6_3.1.x86_64.rpm \
+        /rpm/spice-gtk-0.11-11.el6_3.1.x86_64.rpm \
+        /rpm/spice-gtk-python-0.11-11.el6_3.1.x86_64.rpm \
+        /rpm/spice-server-0.10.1-10.el6.x86_64.rpm \
+        /rpm/which-2.19-6.el6.x86_64.rpm \
+        /rpm/xcb-util-0.3.6-1.el6.x86_64.rpm \
+        /rpm/xorg-x11-drv-evdev-2.6.0-2.el6.x86_64.rpm \
+        /rpm/xorg-x11-drv-vesa-2.3.0-2.el6.x86_64.rpm \
+        /rpm/xorg-x11-drv-void-1.4.0-1.el6.x86_64.rpm \
+        /rpm/xorg-x11-drv-wacom-0.13.0-6.el6.x86_64.rpm \
+        /rpm/xorg-x11-fonts-misc-7.2-9.1.el6.noarch.rpm \
+        /rpm/xorg-x11-fonts-Type1-7.2-9.1.el6.noarch.rpm \
+        /rpm/xorg-x11-font-utils-7.2-11.el6.x86_64.rpm \
+        /rpm/xorg-x11-server-common-1.10.6-1.el6.centos.x86_64.rpm \
+        /rpm/xorg-x11-server-utils-7.5-5.2.el6.x86_64.rpm \
+        /rpm/xorg-x11-server-Xorg-1.10.6-1.el6.centos.x86_64.rpm \
+        /rpm/xorg-x11-xauth-1.0.2-7.1.el6.x86_64.rpm \
+        /rpm/xorg-x11-xinit-1.0.9-13.el6.x86_64.rpm \
+        /rpm/xorg-x11-xkb-utils-7.4-6.el6.x86_64.rpm \
+		/rpm/tar-1.23-7.el6.x86_64.rpm
 ######
 # Note to self.
 # echo "what" | sed 's/ /\

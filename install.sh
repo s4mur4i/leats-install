9cd8a5d858c76f8a0ad118f1ec202e6ea975eee2
s4mur4i <admin@szamuraj.com>
2013-04-21 02:54:57 -0700

    Fixed typo with kernel in grub.conf

--- install.sh
+++ install.sh
@@ -151,7 +151,7 @@ splashimage=(hd0,0)/boot/grub/splash.xpm.gz\n
 hiddenmenu\n
 title CentOS (2.6.32-279.el6.x86_64)\n
 \troot (hd0,0)\n
-\tkernel /boot/vmlinuz-2.6.32-279.el6.9.1.x86_64 ro root=UUID=c13b98e8-f346-40b4-b6f9-e72bf43e99e1 rd_NO_LUKS rd_NO_LVM LANG=en_US.UTF-8 rd_NO_MD SYSFONT=latarcyrheb-sun16 crashkernel=auto  KEYBOARDTYPE=pc KEYTABLE=us rd_NO_DM rhgb quiet\n
+\tkernel /boot/vmlinuz-2.6.32-279.9.1.el6.x86_64 ro root=UUID=c13b98e8-f346-40b4-b6f9-e72bf43e99e1 rd_NO_LUKS rd_NO_LVM LANG=en_US.UTF-8 rd_NO_MD SYSFONT=latarcyrheb-sun16 crashkernel=auto  KEYBOARDTYPE=pc KEYTABLE=us rd_NO_DM rhgb quiet\n
 \tinitrd /boot/initramfs-2.6.32-279.9.1.el6.x86_64.img\n" >$LFS/boot/grub/grub.conf
 grub-install --recheck --root-directory=$LFS $TARGET
 #rpm --root $LFS -vhi
